ff34c3652667f6ebb136e1376bd3ec5c7dbc7932
unhaleshreya <shreyaunhale1@gmail.com>
2025-09-26 19:00:02 +0530

    table

--- resources/views/admin/careers/index.blade.php
+++ resources/views/admin/careers/index.blade.php
@@ -5,13 +5,13 @@
     <div class="content-header">
         <div class="container-fluid">
 
-            <div class="card mt-4">
+      <div class="card mt-4">
                 <div class="card-header">
                     <strong>Submitted Applications</strong>
                 </div>
                 <div class="card-body">
-                    <table class="table table-bordered table-striped">
-                        <thead class="table-dark">
+                    <table class="table table-bordered table-striped" id="careersTable">
+                        <thead>
                             <tr>
                                 <th>sr.no.</th>
                                 <th>First Name</th>
@@ -20,7 +20,7 @@
                                 <th>Phone</th>
                                 <th>Message</th>
                                 <th>Attachment</th>
-                                
+
                             </tr>
                         </thead>
                         <tbody>
@@ -34,14 +34,13 @@
                                     <td>{{ $career->message ?? '-' }}</td>
                                     <td>
                                         @if($career->resume_path)
-                                            <a href="{{ asset('storage/' . $career->attachment) }}" target="_blank" class="btn btn-sm btn-info">
-                                                View File
-                                            </a>
+                                           <a href="{{ asset('storage/' . $career->attachment) }}"
+                                             target="_blank">View File</a>
                                         @else
                                             <span class="text-muted">No File</span>
                                         @endif
                                     </td>
-                                   
+
                                 </tr>
                             @empty
                                 <tr>
@@ -51,7 +50,7 @@
                         </tbody>
                     </table>
 
-                  
+
 
                 </div>
             </div>
@@ -59,3 +58,27 @@
         </div>
     </div>
 </div>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script>
+    $(document).ready(function () {
+        $('#careersTable').DataTable({
+            dom: 'Bfrtip',   // Show buttons (Copy, CSV, Print, etc.)
+            paging: true,    // Enable pagination
+            searching: true, // Enable search
+            ordering: true,  // Enable sorting
+            responsive: true,
+            lengthMenu: [10, 25, 50, 100],
+            buttons: [
+                'copy', 'csv', 'print'
+            ]
+        });
+    });
+    </script>
